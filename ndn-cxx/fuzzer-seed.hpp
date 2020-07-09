944d5dca7a0a218f2a73594637628054b2b74587
gtorresz <gtorresz@nmsu.edu>
2020-07-09 08:35:50 -0600

    adding all fuzzing related changes

---
+++ ndn-cxx/fuzzer-seed.hpp
@@ -0,0 +1,16 @@
+#ifndef FUZZER_SEED_H
+#define FUZZER_SEED_H
+using namespace std;
+/*struct  Bucket {
+	  int HSent=0;
+	    int MSent=0;
+	      int LSent=0;
+	        int HRecv=0;
+		  int MRecv=0;
+		    int LRecv=0;
+};*/
+
+extern unsigned int fuzz_seed;
+
+#endif
+
